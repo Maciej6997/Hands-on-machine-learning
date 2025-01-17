522c668d97703768c94b252ffc5e49ac48fd1ff0
Maciej6997 <Maciej.zakowski.1997@gmail.com>
2025-01-17 18:26:37 +0100

    end exercises

--- Chapter 2 - First Project/Classes.py
+++ Chapter 2 - First Project/Classes.py
@@ -2,26 +2,52 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_array, check_is_fitted
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.base import MetaEstimatorMixin, clone
+import numpy as np
 
 class StandardScalerClone(BaseEstimator, TransformerMixin):
-    
-    def ___init__(self, with_mean = True):
+    def __init__(self, with_mean=True):  # no *args or **kwargs!
         self.with_mean = with_mean
-    
-    def fit(self, X, y = None):
-        X = check_array(X)
-        self.mean_ = X.mean(axis = 0)
-        self.scale_ = X.std(axis = 0)
-        self.n_feauteres_in_ = X.shape[0]
-        return self
-    
+
+    def fit(self, X, y=None):  # y is required even though we don't use it
+        X_orig = X
+        X = check_array(X)  # checks that X is an array with finite float values
+        self.mean_ = X.mean(axis=0)
+        self.scale_ = X.std(axis=0)
+        self.n_features_in_ = X.shape[1]  # every estimator stores this in fit()
+        if hasattr(X_orig, "columns"):
+            self.feature_names_in_ = np.array(X_orig.columns, dtype=object)
+        return self  # always return self!
+
     def transform(self, X):
+        check_is_fitted(self)  # looks for learned attributes (with trailing _)
+        X = check_array(X)
+        if self.n_features_in_ != X.shape[1]:
+            raise ValueError("Unexpected number of features")
+        if self.with_mean:
+            X = X - self.mean_
+        return X / self.scale_
+    
+    def inverse_transform(self, X):
         check_is_fitted(self)
         X = check_array(X)
-        assert self.n_feauteres_in_ == X.shape[1]
-        if self.with_mean:
-            X = X-self.mean_
-        return X / self.scale_
+        if self.n_features_in_ != X.shape[1]:
+            raise ValueError("Unexpected number of features")
+        X = X * self.scale_
+        return X + self.mean_ if self.with_mean else X
+    
+    def get_feature_names_out(self, input_features=None):
+        if input_features is None:
+            return getattr(self, "feature_names_in_",
+                           [f"x{i}" for i in range(self.n_features_in_)])
+        else:
+            if len(input_features) != self.n_features_in_:
+                raise ValueError("Invalid number of features")
+            if hasattr(self, "feature_names_in_") and not np.all(
+                self.feature_names_in_ == input_features
+            ):
+                raise ValueError("input_features ≠ feature_names_in_")
+            return input_features
     
 
 class ClusterSimilarity(BaseEstimator, TransformerMixin):
@@ -41,3 +67,34 @@ class ClusterSimilarity(BaseEstimator, TransformerMixin):
     
     def get_feature_names_out(self, names = None):
         return [f"Similarity {i} cluster" for i in range(self.n_clusters)]
+
+
+
+
+class FeatureFromRegressor(MetaEstimatorMixin, BaseEstimator, TransformerMixin):
+    def __init__(self, estimator):
+        self.estimator = estimator
+
+    def fit(self, X, y=None):
+        estimator_ = clone(self.estimator)
+        estimator_.fit(X, y)
+        self.estimator_ = estimator_
+        self.n_features_in_ = self.estimator_.n_features_in_
+        if hasattr(self.estimator, "feature_names_in_"):
+            self.feature_names_in_ = self.estimator.feature_names_in_
+        return self  # always return self!
+    
+    def transform(self, X):
+        check_is_fitted(self)
+        predictions = self.estimator_.predict(X)
+        if predictions.ndim == 1:
+            predictions = predictions.reshape(-1, 1)
+        return predictions
+
+    def get_feature_names_out(self, names=None):
+        check_is_fitted(self)
+        n_outputs = getattr(self.estimator_, "n_outputs_", 1)
+        estimator_class_name = self.estimator_.__class__.__name__
+        estimator_short_name = estimator_class_name.lower().replace("_", "")
+        return [f"{estimator_short_name}_prediction_{i}"
+                for i in range(n_outputs)]
